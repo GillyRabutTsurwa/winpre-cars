d6fcfbc9ea9e5ef3741041eada226d85c3c50ee3
Gilbert Tsurwa <tsurwagilbert@gmail.com>
2023-01-03 14:49:11 -0500

    Refactored Request to use async IIFE (Consult Last Commit)

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -5,11 +5,11 @@
       <Second />
       <Third />
 
-      <div v-for="currentProduct in products" :key="currentProduct._id">
+      <div v-for="currentProduct in state.products" :key="currentProduct._id">
         {{ currentProduct.title }}
       </div>
 
-      <NuxtLink v-for="currentProduct in products" :key="currentProduct._id" :to="currentProduct.slug.current">
+      <NuxtLink v-for="currentProduct in state.products" :key="currentProduct._id" :to="currentProduct.slug.current">
         {{ currentProduct.title }}
       </NuxtLink>
     </main>
@@ -18,31 +18,23 @@
 </template>
 
 <script setup>
-/**
- * NOTE: for the warning in the template that looks similar to this one: https://lightrun.com/answers/fortawesome-vue-fontawesome-nuxt-3-hydration-mismatch-when-using-ssr
- * particularly, this is a "hydration mismatch issue"
- * i'm hoping that it is something that will be fixed soon, as it doesn't seem as it is an error by me
- * it seems the issue that fixes it is wrapping the root element in the ClientOnly component
- */
+const state = reactive({
+  products: [],
+});
 
-/**
- * NOTE: found this solution here https://sanity.nuxtjs.org/getting-started/usage
- * main site is this (very useful): https://sanity.nuxtjs.org/
- */
-const query = groq`*[_type == "product"]`;
-const { data } = useSanityQuery(query); //NOTE: the data property must be extracted with the .value syntaxe
-console.log(data);
-
-const products = data.value;
-console.log(products);
-console.log(Array.isArray(products));
-// console.log(products.value);
-
-// const urls = products.map((currentProduct) => currentProduct.slug.current);
-
-// console.log(urls);
-// const titles = products.map((currentProduct) => currentProduct);
-// console.log(titles);
+(async () => {
+  try {
+    const query = groq`*[_type == "product"]`;
+    const { data, error } = await useSanityQuery(query);
+    if (error.value) throw new Error(`Error: ${error.value}`);
+    console.log(data);
+    state.prodcuts = data.value;
+    console.log(state.products);
+    console.log(Array.isArray(state.products));
+  } catch (error) {
+    console.log(error);
+  }
+})();
 </script>
 
 <style lang="scss" scoped>
